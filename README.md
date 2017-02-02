8934ad20be8843b527486a85b464edcf4e038073
xavier poh <xvrpke@gmail.com>
2017-02-02 11:31:57 +0800

    update css and add scoreboard

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 [How to write readme - Markdown CheatSheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)  
 [How to write a good readme for github repo!](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2)
 
-# Project Name
-<!---
-Read Me Contents
--->
+# Pong game
+This is a Javascript Pong game for web browsers. Javascript code is written in the script.js file and rendering is done using canvas.
+
+Running the tests
+render()
+Paddle(x, y, width, height)
--- assets/js/script.js
+++ assets/js/script.js
@@ -20,10 +20,9 @@ var render = function () {
   ctx.fillRect(0, 0, width, height)
   playerOne.render()
   playerTwo.render()
-  // ball.render()
 }
 
-// paddle
+// constructor function for paddle
 function Paddle (x, y, width, height) {
   this.x = x
   this.y = y
@@ -33,36 +32,45 @@ function Paddle (x, y, width, height) {
   this.y_speed = 0
 }
 
+//  for addition of difficulty levels in future
 Paddle.prototype.render = function () {
   ctx.beginPath()
-  ctx.fillStyle = '#ffffff'
+  ctx.fillStyle = '#00ff00'
   ctx.fillRect(this.x, this.y, this.width, this.height)
+  // ctx.font = '20px sans-serif'
+  // ctx.fillText(this.score.toString(), 175, 50)
   ctx.closePath()
 }
 
 // Player 1's paddle on the left
 function Player1 () {
   this.paddle = new Paddle(20, 210, 10, 80)
+  this.score = 0
 }
 
 // Player 2's paddle on the right
 function Player2 () {
   this.paddle = new Paddle(670, 210, 10, 80)
+  this.score = 0
 }
 
 Player1.prototype.render = function () {
   this.paddle.render()
+  ctx.font = '25px VT323'
+  ctx.fillText(this.score, 175, 50)
 }
 
 Player2.prototype.render = function () {
   this.paddle.render()
+  ctx.fillText(this.score, 525, 50)
 }
 
+//  constructor function for the ball
 function Ball (x, y) {
   this.x = x
   this.y = y
   this.radius = 5
-  this.x_speed = 3
+  this.x_speed = 5
   this.y_speed = 0
 }
 
@@ -78,12 +86,12 @@ function Ball (x, y) {
 //   ctx.restore()
 // }
 
+//  creating playerOne object as a new instance from Player1 constructor
 var playerOne = new Player1()
-// playerOne.render()
+//  creating playerTwo object as a new instance from Player2 constructor
 var playerTwo = new Player2()
-// playerTwo.render()
+//  creating ball object as a new instance from Ball constructor
 var ball = new Ball(350, 250)
-// ball.render()
 
 Ball.prototype.update = function (paddle1, paddle2) {
   this.x += this.x_speed
@@ -105,20 +113,26 @@ Ball.prototype.update = function (paddle1, paddle2) {
   //  point scored
   if (bottom_x < 5 || top_x + 5 > 700) {
     //  reset ball to start from middle
-    this.x_speed = 3
+    if(bottom_x < 5) {
+      playerTwo.score++
+    }
+    if(top_x + 5 > 700) {
+      playerOne.score++
+    }
+    this.x_speed = 5
     this.y_speed = 0
     this.x = 300
     this.y = 200
   }
   //  hit Player 1's paddle
   if (bottom_x > paddle1.x && top_x < (paddle1.x + paddle1.width) && top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y) {
-    this.x_speed = 3
+    this.x_speed = 5
     this.y_speed += (paddle1.y_speed / 2)
     this.x += this.x_speed
   }
   //  hit Player 2's paddle
   else if (top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x && top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y) {
-    this.x_speed = -3
+    this.x_speed = -5
     this.y_speed += (paddle2.y_speed / 2)
     this.x += this.x_speed
   }
@@ -127,7 +141,7 @@ Ball.prototype.update = function (paddle1, paddle2) {
   ctx.save()  //  set original state of the canvas
   ctx.beginPath()
   ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
-  ctx.fillStyle = '#ffffff'
+  ctx.fillStyle = '#00ff00'
   ctx.fill()
   ctx.closePath()
   ctx.restore() //  restore to saved state
@@ -153,6 +167,7 @@ var keysDown = {}
 //  move paddles when keys are pressed
 window.addEventListener('keydown', function (e) {
   keysDown[e.keyCode] = true
+  e.preventDefault()
 })
 window.addEventListener('keyup', function (e) {
   delete keysDown[e.keyCode]
@@ -162,9 +177,9 @@ playerOne.update = function () {
   for (var key in keysDown) {
     var value = Number(key)
     if (value == 87) { //  W key
-      playerOne.paddle.move(0, -3)
-    } else if (value == 83) {
-      playerOne.paddle.move(0, 3)
+      playerOne.paddle.move(0, -5)
+    } else if (value == 83) { //  S key
+      playerOne.paddle.move(0, 5)
     } else {
       playerOne.paddle.move(0, 0)
     }
@@ -175,9 +190,9 @@ playerTwo.update = function () {
   for (var key in keysDown) {
     var value = Number(key)
     if (value == 38) { //  arrow up key
-      playerTwo.paddle.move(0, -3)
-    } else if (value == 40) {
-      playerTwo.paddle.move(0, 3)
+      playerTwo.paddle.move(0, -5)
+    } else if (value == 40) { //  arrow down key
+      playerTwo.paddle.move(0, 5)
     } else {
       playerTwo.paddle.move(0, 0)
     }
